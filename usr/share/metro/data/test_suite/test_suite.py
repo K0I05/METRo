356c0fd220c567b324d4920d6ba1a490e8f49f91
Sasa Zhang <sasa.zhang@canada.ca>
2019-11-22 17:32:26 +0100

    Update test_suite.py

--- usr/share/metro/data/test_suite/test_suite.py
+++ usr/share/metro/data/test_suite/test_suite.py
@@ -415,7 +415,13 @@ def main():
             if forecast_exists and station_exists and observation_exists:
                 file_output_path = os.path.join(test_suite_path + dir_path[1:], 'roadcast_test_suite_run.xml')
                 os.chdir(test_suite_path)  # Change back the path so as to make the function call.
-                command_to_run = 'python3 ../../../../../src/frontend/metro.py {} --input-forecast {} --input-station {} ' \
+                # command_to_run = 'python3 ../../../../../src/frontend/metro.py {} --input-forecast {} --input-station {} ' \
+                #                  '--input-observation {} --output-roadcast {}'.format(extra_parameter,
+                #                                                                       file_forecast_path,
+                #                                                                       file_station_path,
+                #                                                                       file_observation_path,
+                #                                                                       file_output_path)
+                command_to_run = 'python3 ../../metro.py {} --input-forecast {} --input-station {} ' \
                                  '--input-observation {} --output-roadcast {}'.format(extra_parameter,
                                                                                       file_forecast_path,
                                                                                       file_station_path,
